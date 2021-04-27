8ccb8c715512d4fbe82893fefe6082e4fb061c29
dinhson2905 <dinhson2905@gmail.com>
2021-04-27 15:12:53 +0700

    add pretrained pranet

--- services/libs/pranet/res2net_v1b.py
+++ services/libs/pranet/res2net_v1b.py
@@ -14,12 +14,28 @@ model_urls = {
 
 class Bottle2neck(nn.Module):
     expansion = 4
+
     def __init__(self, inplanes, planes, stride=1, downsample=None, baseWidth=26, scale=4, stype='normal'):
+        """ Constructor
+        Args:
+            inplanes: input channel dimensionality
+            planes: output channel dimensionality
+            stride: conv stride. Replaces pooling layer.
+            downsample: None when stride = 1
+            baseWidth: basic width of conv3x3
+            scale: number of scale.
+            type: 'normal': normal set. 'stage': first block of a new stage.
+        """
         super(Bottle2neck, self).__init__()
+
         width = int(math.floor(planes * (baseWidth / 64.0)))
         self.conv1 = nn.Conv2d(inplanes, width * scale, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(width * scale)
-        self.nums = 1 if scale == 1 else scale - 1
+
+        if scale == 1:
+            self.nums = 1
+        else:
+            self.nums = scale - 1
         if stype == 'stage':
             self.pool = nn.AvgPool2d(kernel_size=3, stride=stride, padding=1)
         convs = []
@@ -29,23 +45,29 @@ class Bottle2neck(nn.Module):
             bns.append(nn.BatchNorm2d(width))
         self.convs = nn.ModuleList(convs)
         self.bns = nn.ModuleList(bns)
+
         self.conv3 = nn.Conv2d(width * scale, planes * self.expansion, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(planes * self.expansion)
+
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stype = stype
         self.scale = scale
         self.width = width
-    
+
     def forward(self, x):
         residual = x
-        out = self.relu(self.bn1(self.conv1(x)))
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
         spx = torch.split(out, self.width, 1)
         for i in range(self.nums):
-            if i == 0 or stype == 'stage':
+            if i == 0 or self.stype == 'stage':
                 sp = spx[i]
             else:
-                sp += spx[i]
+                sp = sp + spx[i]
             sp = self.convs[i](sp)
             sp = self.relu(self.bns[i](sp))
             if i == 0:
@@ -56,14 +78,16 @@ class Bottle2neck(nn.Module):
             out = torch.cat((out, spx[self.nums]), 1)
         elif self.scale != 1 and self.stype == 'stage':
             out = torch.cat((out, self.pool(spx[self.nums])), 1)
+
         out = self.conv3(out)
         out = self.bn3(out)
 
         if self.downsample is not None:
             residual = self.downsample(x)
-        
+
         out += residual
         out = self.relu(out)
+
         return out
 
 
@@ -104,16 +128,27 @@ class Res2Net(nn.Module):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                nn.AvgPool2d(kernel_size=stride, stride=stride, ceil_mode=True, count_include_pad=False),
-                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1, stride=1, bias=False),
+                nn.AvgPool2d(kernel_size=stride, stride=stride,
+                             ceil_mode=True, count_include_pad=False),
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=1, bias=False),
                 nn.BatchNorm2d(planes * block.expansion),
             )
+
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample=downsample, stype='stage', baseWidth=self.baseWidth, scale=self.scale))
+        layers.append(block(self.inplanes, planes, stride, downsample=downsample,
+                            stype='stage', baseWidth=self.baseWidth, scale=self.scale))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes, baseWidth=self.baseWidth, scale=self.scale))
+
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        x = self.maxpool(self.relu(self.bn1(self.conv1(x))))
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
 
         x = self.layer1(x)
         x = self.layer2(x)
@@ -123,6 +158,7 @@ class Res2Net(nn.Module):
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
+
         return x
 
 
@@ -156,7 +192,8 @@ def res2net50_v1b_26w_4s(pretrained=False, **kwargs):
     """
     model = Res2Net(Bottle2neck, [3, 4, 6, 3], baseWidth=26, scale=4, **kwargs)
     if pretrained:
-        model_state = torch.load('/media/nercms/NERCMS/GepengJi/Medical_Seqmentation/CRANet/models/res2net50_v1b_26w_4s-3cf99910.pth')
+        print("Load Res2Net")
+        model_state = torch.load('services/libs/weights/pranet/res2net50_v1b_26w_4s.pth')
         model.load_state_dict(model_state)
         # lib.load_state_dict(model_zoo.load_url(model_urls['res2net50_v1b_26w_4s']))
     return model
@@ -182,3 +219,10 @@ def res2net152_v1b_26w_4s(pretrained=False, **kwargs):
     if pretrained:
         model.load_state_dict(model_zoo.load_url(model_urls['res2net152_v1b_26w_4s']))
     return model
+
+
+if __name__ == '__main__':
+    images = torch.rand(1, 3, 224, 224).cuda(0)
+    model = res2net50_v1b_26w_4s(pretrained=True)
+    model = model.cuda(0)
+    print(model(images).size())
--- services/predict_service.py
+++ services/predict_service.py
@@ -11,13 +11,16 @@ import torch
 import torch.nn.functional as F
 from torchvision.transforms import transforms
 from .libs.hardmseg import HarDCPD, HarDMSEG
+from .libs.pranet import PraNet
 from datetime import datetime
 from .utils import convert_base64_to_image
 import base64
 
 weights_hardmseg = 'static/checkpoints/hardmseg.pth'
 weights_cpd = 'static/checkpoints/hardcpd.pth'
+weights_pranet = 'static/checkpoints/pranet.pth'
 image_output = 'static/images'
+
 
 class PredictService():
     def __init__(self):
@@ -69,6 +72,23 @@ class PredictService():
             model.eval()
             image_tensor = image_tensor.to(device)
             _, res = model(image_tensor)
+            res = F.upsample(res, size=(width, height), mode='bilinear', align_corners=True)
+            res = res.sigmoid().data.cpu().numpy().squeeze()
+            res = (res - res.min()) / (res.max() - res.min() + 1e-8)
+            imageio.imwrite(ln, res)
+            response_json = {
+                'message': 'Success',
+                'name': saved_image_name,
+                'base64_encode': base64.b64encode(open(ln, 'rb').read()).decode('utf-8')
+            }
+        elif model_name == 'pranet':
+            print("Model: PraNet")
+            model = PraNet()
+            model.load_state_dict(torch.load(weights_pranet))
+            model.to(device)
+            model.eval()
+            image_tensor = image_tensor.to(device)
+            _, _, _, res = model(image_tensor)
             res = F.upsample(res, size=(width, height), mode='bilinear', align_corners=True)
             res = res.sigmoid().data.cpu().numpy().squeeze()
             res = (res - res.min()) / (res.max() - res.min() + 1e-8)
